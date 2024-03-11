f45b9a4da36fdceb69667fe21367b56df83bdea0
Alessandro Borges <adborgis@gmail.com>
2024-03-11 13:24:49 -0300

    add arquivo teste

---
+++ teste.py
@@ -0,0 +1,10 @@
+# while condicao " for true é implicito"
+
+# comando para abortar execucão de algo no terminal/program, eh só dar "ctrl c"
+
+import time
+
+condicao = True
+while condicao:
+    print("execute minha etl")
+    time.sleep(5)
